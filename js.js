13a29eaa0a947cfa2ad074ecf137a7ae2ddc3607
troanart <128319062+troanart@users.noreply.github.com>
2023-09-27 14:53:14 +0300

    New version add audio if you win

--- js.js
+++ js.js
@@ -10,52 +10,36 @@
 3-Помните про свойства, получения соседних элементов (previousElementSibling, previousElementSibling и т.д)
 
 */
-// const letterA = document.querySelector(".box_a");
-// const letterB = document.querySelector(".box_b");
-// const letterC = document.querySelector(".box_c");
-// const letterD = document.querySelector(".box_d");
-// const letterE = document.querySelector(".box_e");
-// const letterF = document.querySelector(".box_f");
-
-// letterA.addEventListener("click", function () {
-//   if (letterA.previousElementSibling != null) {
-//     letterA.previousElementSibling.before(letterA);
-//   } else if (letterA.nextElementSibling != null) {
-//     letterA.nextElementSibling.after(letterA);
-//   }
-// });
-
-// letterB.addEventListener("click", function () {
-//   letterB.previousElementSibling.before(letterB);
-// });
-
-// letterC.addEventListener("click", function () {
-//   letterC.previousElementSibling.before(letterC);
-// });
-
-// letterD.addEventListener("click", function () {
-//   letterD.previousElementSibling.before(letterD);
-// });
-
-// letterE.addEventListener("click", function () {
-//   letterE.previousElementSibling.before(letterE);
-// });
-
-// letterF.addEventListener("click", function () {
-//   letterF.previousElementSibling.before(letterF);
-// });
 
 const letters = document.querySelectorAll(".boxes__box");
 const audioClick = new Audio("audio/Mountain Audio - Menu Click.mp3");
+const audioWin = new Audio("audio/huge win.wav");
+let newLetters;
 
 for (let i = 0; i < letters.length; i++) {
   letters[i].addEventListener("click", function () {
     if (letters[i].previousElementSibling != null) {
       letters[i].previousElementSibling.before(letters[i]);
       audioClick.play();
+      newLetters = document.querySelectorAll(".boxes__box");
+
+      console.log(letters);
     } else if (letters[i].nextElementSibling != null) {
       letters[i].nextElementSibling.after(letters[i]);
       audioClick.play();
+      newLetters = document.querySelectorAll(".boxes__box");
+    }
+    if (
+      newLetters[0].classList.contains("box_a") &&
+      newLetters[1].classList.contains("box_b") &&
+      newLetters[2].classList.contains("box_c") &&
+      newLetters[3].classList.contains("box_d") &&
+      newLetters[4].classList.contains("box_e") &&
+      newLetters[5].classList.contains("box_f")
+    ) {
+      audioWin.play();
     }
   });
 }
+
+//
